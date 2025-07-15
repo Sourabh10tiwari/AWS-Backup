5ab06ea88eb4bd6581fa0ea422aed9baca5628a1
root <root@ip-172-31-80-9.ec2.internal>
2025-07-15 06:42:08 +0000

    Updated s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,11 +2,14 @@ resource "aws_s3_bucket" "versioned_bucket" {
   bucket = "versioned-bucket-34785"
   force_destroy = true
 
-  versioning {
-    enabled = true
-  }
-
   tags = {
     Name = "versioned-bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.versioned_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
